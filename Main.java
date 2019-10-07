481a573facd70c7306999cb98aa4d0fc529f14de
nuggex <nuggex@gmail.com>
2019-10-07 20:39:53 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,41 @@
+package com.nugge;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("TeMpErAtUrANALySaToRn 9001");
+        temperatures();
+
+
+    }
+    private static double[] temperatures(){
+        System.out.println("Hur många temperaturer vill du mata in?");
+        double[] temps = new double[inputi()];
+        for(int i = 0; i<temps.length;i++){
+            System.out.println("Ange temperaturvärde "+ (i+1) + ": ");
+            double intoarr = inputd();
+            if (intoarr < -51 || intoarr > 60) {
+                System.out.println("De ha nu alder vari sådandär temperatur!");
+                i--;
+            } else {
+                temps[i] = intoarr;
+        }}
+        return temps;
+    }
+
+    private static double inputd(){
+        Scanner in = new Scanner(System.in);
+        return in.nextDouble();
+    }
+    private static  int inputi(){
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+
+}
+
